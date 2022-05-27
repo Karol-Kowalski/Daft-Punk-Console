8b5c5f462d67850645c256340c18f89b5b29f432
Virion <kajko195@tlen.pl>
2022-05-27 23:08:20 +0200

    removing .playing class from .key
    Wrap all key elements using querySelectorAll and make loop for every
    element. In loop add event listener for every element which
    execute function for remove .playing class after transition.
    
    Add small corection in CSS and HTML files.

--- console.js
+++ console.js
@@ -1,12 +1,22 @@
+function removeTransition (e) {
+  if(e.propertyName !== 'transform') return;
+  
+  this.classList.remove('playing');
+};
+
 function playSound(e) {
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if(!key) return;
+  key.classList.add('playing')
 
   const audio = document.getElementById('sound');
   audio.src = `sounds/sounds_${key.getAttribute('data-url')}.wav`;
 
-  audio.currentTime = 0;
+  audio.currentTime = 0; //restart play sound on every key click
   audio.play();
 };
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 window.addEventListener('keydown', playSound);
