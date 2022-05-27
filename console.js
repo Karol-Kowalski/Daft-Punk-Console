c4b811c3e9114e0dba6aceb66e1b0ad5b2f1afb3
Virion <kajko195@tlen.pl>
2022-05-27 22:14:41 +0200

    play sounds
    Add audio files.
    Wrapped audio html element and change audio src on every key down to
    right sound and play it.

--- console.js
+++ console.js
@@ -2,6 +2,11 @@ function playSound(e) {
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if(!key) return;
 
+  const audio = document.getElementById('sound');
+  audio.src = `sounds/sounds_${key.getAttribute('data-url')}.wav`;
+
+  audio.currentTime = 0;
+  audio.play();
 };
 
 window.addEventListener('keydown', playSound);
